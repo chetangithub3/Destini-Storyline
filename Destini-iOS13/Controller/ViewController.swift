66ed868dac88aada3caca188cd359d9c05b54eea
Chetan <chetan.getsmail@gmail.com>
2022-03-03 17:11:22 -0500

    MVC on a storyline based activity

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story.destini[story.getStoryNumber()].title
+        choice1Button.setTitle(story.destini[story.getStoryNumber()].choice1, for: .normal)
+        choice2Button.setTitle(story.destini[story.getStoryNumber()].choice2, for: .normal)
+        
+        
     }
 
-
+    @IBAction func choiceMade(_ sender: Any) {
+        let routeChosen = story.chooseRoute(userAnswer: sender as! UIButton)
+        
+        updateUI(route: routeChosen)
+        
+    }
+    
+    func updateUI(route: Story?){
+        
+        
+        storyLabel.text = route?.title
+        choice1Button.setTitle(route?.choice1, for: .normal)
+        choice2Button.setTitle(route?.choice2, for: .normal)
+        
+        
+    }
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -7,5 +7,48 @@
 //
 
 import Foundation
+import UIKit
 
+struct StoryBrain{
+    
+    let destini: [Story] =  [Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+                             Story(title: "You see a tiger", choice1: "Shout aloud", choice2: "Play dead"),
+                             Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")]
+    var storyNumber: Int = 0
+    
+    func getStoryTitle(storyNumber: Int) -> String{
+        return destini[storyNumber].title
+    }
+    
+    func getChoice1(storyNumber: Int) -> String{
+        return destini[storyNumber].choice1
+    }
+    
+    func getChoice2(storyNumber: Int) -> String{
+        return destini[storyNumber].choice2
+    }
+    
+    func getStoryNumber() -> Int{
+        return storyNumber
+    }
+    
+    
+   
+    
+    mutating func chooseRoute(userAnswer: UIButton) -> Story{
+        if userAnswer.titleLabel?.description == destini[storyNumber].choice1{
+            storyNumber += 1
+            print(destini[storyNumber])
+            return destini[storyNumber]
+        }
+        else{
+            storyNumber += 2
+            return destini[self.storyNumber]
+        }
+    }
+    
+    
+    
+    
+}
 
